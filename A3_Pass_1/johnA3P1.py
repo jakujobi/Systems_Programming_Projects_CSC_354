14c73ed29da4bab544725409d4930e63a0ecf1a2
John <john@jakujobi.com>
2024-10-28 19:49:33 -0500

    Refactor import statements in johnA3P1.py

--- A3_Pass_1/johnA3P1.py
+++ A3_Pass_1/johnA3P1.py
@@ -2,9 +2,9 @@ import sys
 import os
 import re
 
-from symbol_table_builder import SymbolTableBuilder, Validator, SymbolData, SymbolTable
-from OpcodeHandler import OpcodeHandler
-from SourceCodeLine import SourceCodeLine
-from ErrorLogHandler import ErrorLogHandler
-from FileExplorer import FileExplorer
-from Literal_table_builder import LiteralTableBuilder
+from symbol_table_builder import *
+from OpcodeHandler import *
+from SourceCodeLine import *
+from ErrorLogHandler import *
+from FileExplorer import *
+from Literal_table_builder import *
