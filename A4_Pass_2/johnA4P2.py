67cf7be5fe929edf037f71f9830136ffa3207417
John <john@jakujobi.com>
2024-11-14 22:41:02 -0600

    Rename instance of AssemblerPass1 to AssemblerPass2 in johnA4P2.py

--- A4_Pass_2/johnA4P2.py
+++ A4_Pass_2/johnA4P2.py
@@ -7,14 +7,14 @@ from pathlib import Path
 repo_home_path = Path(__file__).resolve().parent.parent
 sys.path.append(str(repo_home_path))
 
-from Modules.AssemblerPass2 import AssemblerPass1
+from Modules.AssemblerPass2 import AssemblerPass2
 
 if __name__ == "__main__":
     # Get the source file from the command-line argument or use the default
     source_file = sys.argv[1] if len(sys.argv) > 1 else "source.asm"
     
     # Create an instance of AssemblerPass1 with the source file
-    assembler_pass1 = AssemblerPass2(source_file)
+    assembler_pass2 = AssemblerPass2(source_file)
     
     # Run the assembler pass 1
-    assembler_pass1.run()
+    assembler_pass2.run()
