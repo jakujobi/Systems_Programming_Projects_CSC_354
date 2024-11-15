a853ea4e37a4b033f23b49f44a2a8d515e1e5fb9
John <john@jakujobi.com>
2024-11-14 22:31:55 -0600

    Refactor AssemblerPass2 constructor to include source_file_path and additional parameters

--- Modules/AssemblerPass2.py
+++ Modules/AssemblerPass2.py
@@ -15,5 +15,6 @@ from Modules.Literal_Table_Builder import *
 from Modules.FileExplorer import *
 
 class AssemblerPass2:
-    def __init__(self):
+    def __init__(self, source_file_path: str, logger: ErrorLogHandler = None, character_literal_prefix: str = '0C', hex_literal_prefix: str = '0X', allow_error_lines_in_generated_document: bool = True, stop_on_error: bool = False, generated_file_extension: str = '.int'):
+        self.source_file_path = source_file_path
         pass
