b6de21dd6bc2bec459ad6fc3a515b6b67d4ae5af
John <john@jakujobi.com>
2024-11-25 20:28:54 -0600

    Refactor assembly code in source.asm: Update instructions, improve formatting, and enhance readability

--- A3_Pass_1/source.asm
+++ A3_Pass_1/source.asm
@@ -11,7 +11,7 @@ LOOP:   ADDR   A, B
         DIVR   SW, PC
         AND    DATA
         OR     COUNT
-        SHIFTL B, 4
+        SHIFTL Y, 4
         SHIFTR F, P .bad
         TIXR   T
         CLEAR  X
@@ -31,12 +31,16 @@ HERE:   +LDF   #FLOATZ
         ADDF   RETADR
         ADD    #2
         DIV    3
-        MUL    #5
-        SUB    4
+        MUL    #5-4
+        SUB    HERE-LOOP
+        LDA    ALPHA+LOOP
         SUBF   =0x45
         DIVF   =0cdw
+        SUBF   0xf3
+        DIVF   =0xCs
         COMPF  DATa
-        J      LOOP
+        EXTREF GOTEM
+        J      GOTEM
         JLT    LOOP
         JGT    LOOP
         +TD    DEVICE
@@ -51,7 +55,7 @@ HERE:   +LDF   #FLOATZ
         STT    #ONE,X
         STX    @ONE,X
         NORM
-        SVC    1
+        RMO    B
         SVC    #HERE
         SIO
 EQUAL:  EQU    HERE
@@ -69,7 +73,7 @@ BETA:   EQU    ALPHA
         ADD    HERE+#16
         ADD    #HERE+ALPHA
         TIO
-        RMO    B
+        SVC    @ALPHA
         LPS    BREAK
         SSK    DEVICE
         COMPR  A, 5 .bad
@@ -84,11 +88,13 @@ PRINT:  LDS    LENGTH
         LDL    CHARZ
         RSUB
 
-DATA:   BYTE   =0Xf1
+. ha BLANK
+DATA:   BYTE   0Xf1
+DATA:   BYTE   0XF1
 DATA:   BYTE   =0XF1
-DA_2:   BYTE   =0XF11
-DAT3:   BYTE   =0XG1
-DAT4:   BYTE   =X'G1'
+DA_2:   BYTE   0XF11
+DAT3:   BYTE   0XG1
+DAT4:   BYTE   X'G1'
 !BAD:   BYTE   d
 1BAD:   BYTE   =
 _GOOD:  BYTE   1
@@ -98,16 +104,19 @@ GOO:    BYTE   1
 BA3D:   BYTE   #1
 BA#D:   BYTE   1
 Z:      BYTE   20
-CHARZ:  BYTE   =0CA
-FLOATZ: BYTE   =0C'\"1.dD
+CHARZ:  BYTE   0CA
+FLOATZ: BYTE   0C'\"1.dD
 FOUR:   RESB   #4
 HABR:   RESW   637     .should make anything past use B reg
 COUNT:  WORD   #1
 LENGTH: WORD   4096
+        WORD   COUNT
+        WORD   COUNT-HABR
 ONE:    RESB   1
 RETaDR: RESW   #1
+FIVE:	        RESW	#3		. partial line comment - ignore (actual tabs here)
 BREAK:  RESb   1911     .should make anything past use format 4
-DEVICE: BYTE   =0X'\"F8'
+DEVICE: BYTE   0X'\"F8'
         RESB   DATA
         RESB   HERE-ALPHA
         RESB   ALpHA-7777
@@ -123,9 +132,22 @@ ZI:     EQU    16+HERE
 ZJ:     EQU    HERE+#16
 ZK:     EQU    #HERE+ALPHA
 ZL:     EQU
-        EQU    heir
+TEST:   EQU    heir
+T2:     EQU    *
         EQU    HERE!
         EQU    #HERE
-        EXTDEF ENDFUN
-        EXTREF OTRFUN
-        END    FIRST
+ZZ:     EQU    =0CABC
+        EXTDEF ZA, ZB,DEVICE, ZD,ZE,ZF,AH,Zz
+        EXTDEF ZG
+        EXTREF OTRFUN,AAA ,AAB
+        WORD   DEVICE-AAA
+        WORD   AAA-AAB
+        JSUB   PRINT
+        JSUB   TEST
+        JSUB   OTRFUN
+        +JSUB  OTRFUN
+        LDA    OTRFUN
+        +LDA    TEST
+        LDA   =0x45
+        LDA   =0Cdw
+        END    PROG
