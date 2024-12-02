5a0803247bf734ff3557e1098079435d2c75d732
John <john@jakujobi.com>
2024-12-02 02:39:39 -0600

    Refactor address formatting in LiteralTableList: change address display to left-align with fixed width

--- Modules/Literal_Table_Builder.py
+++ Modules/Literal_Table_Builder.py
@@ -244,7 +244,8 @@ class LiteralTableList:
         current = self.head
         while current is not None:
             literal_data = current.literal_data
-            address = f"{literal_data.address:05X}" if literal_data.address is not None else "None"
+            # address = f"{literal_data.address:05X}" if literal_data.address is not None else "None"
+            address = f"{literal_data.address:<05}" if literal_data.address is not None else "None"
             result.append(f"{literal_data.name:<10} {literal_data.value:<10} {literal_data.length:<6} {address:<8}")
             current = current.next
         
