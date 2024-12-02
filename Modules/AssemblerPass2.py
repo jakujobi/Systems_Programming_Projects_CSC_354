7a2f14074d4be274df4b7e0471d0e1a27f847204
John <john@jakujobi.com>
2024-12-02 00:24:39 -0600

    Refactor AssemblerPass2 constructor to improve parameter organization and enhance default value handling

--- Modules/AssemblerPass2.py
+++ Modules/AssemblerPass2.py
@@ -13,10 +13,19 @@ from Modules.ErrorLogHandler import *
 from Modules.Symbol_Table_Builder import *
 from Modules.Literal_Table_Builder import *
 from Modules.FileExplorer import *
+from Modules.IntermediateFileParser import *
 
 
 class AssemblerPass2:
-    def __init__(self, int_filename: str, logger: ErrorLogHandler = None, character_literal_prefix: str = '0C', hex_literal_prefix: str = '0X', allow_error_lines_in_generated_document: bool = True, stop_on_error: bool = False, generated_file_extension: str = '.int'):
+    def __init__(self, int_filename: str,
+                 file_explorer: FileExplorer = None,
+                 logger: ErrorLogHandler = None,
+                 Program_length_prefix_for_Hex: str = "Program Length (HEX):",
+                 Program_length_prefix_for_Decimal: str = "Program Length (DEC):",
+                 character_literal_prefix: str = '0C',
+                 hex_literal_prefix: str = '0X',
+                 allow_error_lines_in_generated_document: bool = True):
+        
         self.int_file_name = int_filename
         self.int_file_content = []
 
@@ -29,8 +38,8 @@ class AssemblerPass2:
         self.program_start_address = 0
         self.program_length = 0
         
-        self.Program_length_prefix_for_Hex = "Program Length (HEX):" or Program_length_prefix_for_Hex
-        self.Program_length_prefix_for_Decimal = "Program Length (DEC):" or Program_length_prefix_for_Decimal
+        self.Program_length_prefix_for_Hex = Program_length_prefix_for_Hex or "Program Length (HEX):"
+        self.Program_length_prefix_for_Decimal = Program_length_prefix_for_Decimal or "Program Length (DEC):"
         
         self.Start_div_symbol_table = "===SYM_START==="
         self.End_div_symbol_table = "===SYM_END==="
@@ -39,14 +48,20 @@ class AssemblerPass2:
         self.Start_div_program_length = "===PROG_LEN_START==="
         self.End_div_program_length = "===PROG_LEN_END==="
         
-        pass
+        self.character_literal_prefix = character_literal_prefix or '0C'
+        self.hex_literal_prefix = hex_literal_prefix or '0X'
+        
+        self.file_explorer = file_explorer or FileExplorer()
+        self.logger = logger or ErrorLogHandler()
+        
+        
     def run(self):
         pass
     def load_immediate_file(self):
         """
         Loads the source code from the file into int_file_content list.
         """
-        self.int_file_content = self.FileExplorer.read_file_raw(self.int_file_name)
+        self.int_file_content = self.file_explorer.read_file_raw(self.int_file_name)
         # Check if the file is empty
         if not self.int_file_content:
             self.logger.log_error(f"The file '{self.int_file_name}' is empty.")
