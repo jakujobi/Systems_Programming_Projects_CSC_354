0645a8f45322c6311795d9ce2eff2044def65cc0
John <john@jakujobi.com>
2024-12-04 00:58:26 -0600

    Add A5_Combo.py to initialize and run ComboAssembler for source file assembly

---
+++ A5_Combo/A5_Combo.py
@@ -0,0 +1,23 @@
+import os
+import re
+import sys
+from pathlib import Path
+
+
+from Modules.ComboAssembler import ComboAssembler
+
+# Add the parent directory to the system path
+repo_home_path = Path(__file__).resolve().parent.parent
+sys.path.append(str(repo_home_path))
+
+from Modules.AssemblerPass2 import AssemblerPass2
+
+if __name__ == "__main__":
+    # Get the source file from the command-line argument or use the default
+    source_file = sys.argv[1] if len(sys.argv) > 1 else "source.int"
+    
+    # Create an instance of AssemblerPass1 with the source file
+    ComboAssembler = ComboAssembler(source_file)
+    
+    # Run the assembler pass 1
+    ComboAssembler.assemble()
