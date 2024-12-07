a6f22b0c09bb51d2a29719c0bcb6201eaa4fadc5
John <john@jakujobi.com>
2024-12-07 01:16:59 -0600

    Use a hardcoded default source file in main if no command-line argument is provided.

--- A3_Pass_1/johnA3P1.py
+++ A3_Pass_1/johnA3P1.py
@@ -10,8 +10,9 @@ sys.path.append(str(repo_home_path))
 from Modules.AssemblerPass1 import AssemblerPass1
 
 if __name__ == "__main__":
+    hardcoded_file = "Htest.asm"
     # Get the source file from the command-line argument or use the default
-    source_file = sys.argv[1] if len(sys.argv) > 1 else "source.asm"
+    source_file = sys.argv[1] if len(sys.argv) > 1 else hardcoded_file
     
     # Create an instance of AssemblerPass1 with the source file
     assembler_pass1 = AssemblerPass1(source_file)
