1920eae76b0690abdd67b009a3bd3784d13316c8
John <john@jakujobi.com>
2024-12-08 01:20:37 -0600

    Add error handling in process_files to manage file processing issues

--- A4_Pass_2/ComboP1P2.py
+++ A4_Pass_2/ComboP1P2.py
@@ -14,23 +14,26 @@ from Modules.AssemblerPass2 import AssemblerPass2
 
 def process_files(file_list):
     for file in file_list:
-        source_file = str(file) + ".asm"
-        intermediate_file = str(file) + ".int"
+        try:
+            source_file = str(file) + ".asm"
+            intermediate_file = str(file) + ".int"
 
-        pass1 = AssemblerPass1(source_file)
-        
-        # Ask for user input to proceed with Pass 2
-        proceed = input(f"Proceed with Pass 2 for {intermediate_file}? (y/n): ") # accept enter
-        if proceed.lower() == "y" or proceed == "":
-            pass2 = AssemblerPass2(intermediate_file)
-        else:
-            print("Pass 2 skipped.")
+            pass1 = AssemblerPass1(source_file)
+            
+            # Ask for user input to proceed with Pass 2
+            proceed = input(f"Proceed with Pass 2 for {intermediate_file}? (y/n): ") # accept enter
+            if proceed.lower() == "y" or proceed == "":
+                pass2 = AssemblerPass2(intermediate_file)
+            else:
+                print("Pass 2 skipped.")
+        except:
+            print("Error processing file: ", file)
 
 if __name__ == "__main__":
     # Make an array of file names
-    test_files = ["Htest1",
+    test_files = ["Htest3",
                   "Htest2",
-                  "Htest3",
+                  "Htest1",
                   "t1",
                   "t2",
                   "t3",
