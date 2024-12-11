9c46c812751343d976c8a7c163a18007530b74b6
John <john@jakujobi.com>
2024-12-11 01:36:07 -0600

    Refactor file processing logic into a separate function for improved readability and error handling

--- A4_Pass_2/ComboP1P2.py
+++ A4_Pass_2/ComboP1P2.py
@@ -7,27 +7,31 @@ from pathlib import Path
 repo_home_path = Path(__file__).resolve().parent.parent
 sys.path.append(str(repo_home_path))
 
-from Modules.ComboAssembler import ComboAssembler
-from Modules.ErrorLogHandler import ErrorLogHandler
 from Modules.AssemblerPass1 import AssemblerPass1
 from Modules.AssemblerPass2 import AssemblerPass2
 
 def process_files(file_list):
     for file in file_list:
         try:
-            source_file = str(file) + ".asm"
-            intermediate_file = str(file) + ".int"
-
-            AssemblerPass1(source_file)
-            
-            # Ask for user input to proceed with Pass 2
-            proceed = input(f"Proceed with Pass 2 for {intermediate_file}? (y/n): ") # accept enter
-            if proceed.lower() == "y" or proceed == "":
-                AssemblerPass2(intermediate_file)
-            else:
-                print("Pass 2 skipped.")
+            process_single_file(file)
         except Exception as e:
             print(f"Error processing {file}: {e}")
+            
+def process_single_file(file):
+    try:
+        source_file = str(file) + ".asm"
+        intermediate_file = str(file) + ".int"
+
+        AssemblerPass1(source_file)
+        
+        # Ask for user input to proceed with Pass 2
+        proceed = input(f"Proceed with Pass 2 for {intermediate_file}? (y/n): ") # accept enter
+        if proceed.lower() in ["y", ""]:
+            AssemblerPass2(intermediate_file)
+        else:
+            print("Pass 2 skipped.")
+    except Exception as e:
+        print(f"Error processing {file}: {e}")
 
 if __name__ == "__main__":
     # Make an array of file names
@@ -39,16 +43,9 @@ if __name__ == "__main__":
                 "t2",
                 "t3",
                 ]
-
-    process_files(test_files)
-    # source_file = str(test_files[0]) + ".asm"
-    # intermediate_file = str(test_files[0]) + ".int"
-
-    # pass1 = AssemblerPass1(source_file)
     
-    # # Ask for user input to proceed with Pass 2
-    # proceed = input(f"Proceed with Pass 2 for {intermediate_file}? (y/n): ") # accept enter
-    # if proceed.lower() == "y" or proceed == "":
-    #     pass2 = AssemblerPass2(intermediate_file)
-    # else:
-    #     print("Pass 2 skipped.")
+    if len(sys.argv) > 1:
+        file = sys.argv[1]
+        process_single_file(file)
+    else:
+        process_files(test_files)
